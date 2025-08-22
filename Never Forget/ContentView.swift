06dd8381dbb0d0437aa94d81c9289af60c3fb107
Tomas Cora <tomcora@caddycompanionapp.com>
2025-08-21 22:03:19 -0400

    Initial Commit

---
+++ Never Forget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Never Forget
+//
+//  Created by Tomas Cora on 8/21/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Never ForgetTests/Never_ForgetTests.swift
@@ -0,0 +1,17 @@
+//
+//  Never_ForgetTests.swift
+//  Never ForgetTests
+//
+//  Created by Tomas Cora on 8/21/2025.
+//
+
+import Testing
+@testable import Never_Forget
+
+struct Never_ForgetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
